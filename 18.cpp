12b0a543472eb9cbece0f545ae82c7cc596d848a
nKadykov <nkadykov@gmail.com>
2023-11-27 23:41:01 +0300

    init

---
+++ 18.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> output;
+        for(int i=0; i<n-3; i++){
+            for(int j=i+1; j<n-2; j++){
+                long long newTarget = (long long)target - (long long)nums[i] - (long long)nums[j];
+                int low = j+1, high = n-1;
+                while(low < high){
+                    if(nums[low] + nums[high] < newTarget){
+                        low++;
+                    }
+                    else if(nums[low] + nums[high] > newTarget){
+                        high--;
+                    }
+                    else{
+                        output.push_back({nums[i], nums[j], nums[low], nums[high]});
+                        int tempIndex1 = low, tempIndex2 = high;
+                        while(low < high && nums[low] == nums[tempIndex1]) low++;
+                        while(low < high && nums[high] == nums[tempIndex2]) high--;
+                    }
+                }
+                while(j+1 < n && nums[j] == nums[j+1]) j++;
+            }
+            while(i+1 < n && nums[i] == nums[i+1]) i++;
+        }
+        return output;
+    }
+};
+
+int main() {
+    vector<int> nums1 {1, 0, -1, 0, -2, 2};
+    vector<int> nums2 {2, 2, 2, 2, 2};
+    int target1 {0};
+    int target2 {8};
+
+    Solution solution;
+
+    vector<vector<int>> solution1 = solution.fourSum(nums1, target1);
+    vector<vector<int>> solution2 = solution.fourSum(nums2, target2);
+
+    for(auto x : solution1) {
+        for(auto y : x) {
+            cout << y << ' ';
+        }
+        cout << '\n';
+    }
+    cout << '\n';
+    for(auto x : solution2) {
+        for(auto y : x) {
+            cout << y << ' ';
+        }
+        cout << '\n';
+    }
+ 
+    return 0;
+}
