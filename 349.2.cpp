6e940aab2d4b74a1d55c5eca9a8603143722f1c9
nKadykov <nkadykov@gmail.com>
2024-01-03 11:15:11 +0300

    init

---
+++ 349.2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int l1 = nums1.size();
+        int l2 = nums2.size();
+        int i = 0;
+        int j = 0;
+        vector<int> result;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        while(i < l1 && j < l2) {
+            if(nums1[i] < nums2[j]) {
+                i++;
+            }
+            else if(nums1[i] == nums2[j]) {
+                result.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i] > nums2[j]) {
+                j++;
+            }
+        }
+        result.erase(unique(result.begin(), result.end()), result.end());
+        return result;
+    }
+};
+
+int main() {
+    vector<int> nums11 {1,2,2,1};
+    vector<int> nums12 {2,2};
+    vector<int> nums21 {4,9,5};
+    vector<int> nums22 {9,4,9,8,4};
+
+    Solution solution;
+
+    vector<int> solution1 = solution.intersection(nums11, nums12);
+    vector<int> solution2 = solution.intersection(nums21, nums22);
+
+    for(int x : solution1) {
+        cout << x << ' ';
+    }
+    cout << '\n';
+    for(int x : solution2) {
+        cout << x << ' ';
+    }
+
+    return 0;
+}
