9ee6186e75ac189d1acd12f7cdd6129e09171501
nKadykov <nkadykov@gmail.com>
2024-03-05 20:35:27 +0300

    init

---
+++ 647.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+class Solution {
+    std::string t;
+    int check(int l, int r, int result = 0) {
+        while(l >= 0 && r <= t.size()) {
+            if(t[l--] == t[r++]) {
+                result++;
+            }
+            else {
+                break;
+            }
+        }
+        return result;
+    }
+public:
+    int countSubstrings(std::string s) {
+        int n = s.size();
+        int result = 0;
+        t = s;
+        for(int i = 0; i < n; i++) {
+            result += check(i, i);
+            result += check(i, i + 1);
+        }
+        return result;
+    }
+};
+
+int main() {
+    std::string s1 = "abc";
+    std::string s2 = "aaa";
+
+    Solution solution;
+
+    int solution1 = solution.countSubstrings(s1);
+    int solution2 = solution.countSubstrings(s2);
+
+    std::cout << solution1 << std::endl;
+    std::cout << solution2;
+
+    return 0;
+}
