a3f8b21910a38ea3c079633193298759dc27d283
nKadykov <nkadykov@gmail.com>
2024-08-18 19:40:48 +0300

    fix: 31

--- 31.cpp
+++ 31.cpp
@@ -1,37 +1,44 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-using namespace std;
 
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
+    void nextPermutation(std::vector<int>& nums) 
+    {
         int number = nums.size();
         int index = -1;
-        for(int i = 0; i < number - 1; i++) {
-            if(nums[i] < nums[i + 1]) {
+        for(int i = 0; i < number - 1; i++) 
+        {
+            if(nums[i] < nums[i + 1]) 
+            {
                 index = i;
             }
         }
-        if(index == -1) {
+        if(index == -1) 
+        {
             reverse(nums.begin(), nums.end());
             return;
         }
         int left = index + 1;
-        for(int i = index + 1; i < number; i++) {
-            if(nums[i] > nums[index]) {
+        for(int i = index + 1; i < number; i++) 
+        {
+            if(nums[i] > nums[index]) 
+            {
                 left = i;
             }
         }
-        swap(nums[index], nums[left]);
+        std::swap(nums[index], nums[left]);
         reverse(nums.begin() + 1 + index, nums.end());
         return;
     }
 };
 
-int main() {
-    vector<int> nums1 {1, 2, 3};
-    vector<int> nums2 {3, 2, 1};
-    vector<int> nums3 {1, 1, 5};
+int main() 
+{
+    std::vector<int> nums1 {1, 2, 3};
+    std::vector<int> nums2 {3, 2, 1};
+    std::vector<int> nums3 {1, 1, 5};
 
     Solution solution;
 
@@ -40,14 +47,14 @@ int main() {
     solution.nextPermutation(nums3);
 
     for(auto& x : nums1) {
-        cout << x << ' ';
+        std::cout << x << ' ';
     }
-    cout << '\n';
+    std::cout << '\n';
     for(auto& x : nums2) {
-        cout << x << ' ';
+        std::cout << x << ' ';
     }
-    cout << '\n';
+    std::cout << '\n';
     for(auto& x : nums3) {
-        cout << x << ' ';
+        std::cout << x << ' ';
     }
 }
